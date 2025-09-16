818f0a3dc122887941fdbeabddc0a2aa4f13f5b9
Tim Curtis <tim@moodeaudio.org>
2025-09-16 06:13:51 -0400

    [install.sh] Fix kernel version

--- moode-player/update9/install.sh
+++ moode-player/update9/install.sh
@@ -45,8 +45,8 @@ python3-libupnpp=0.26.1-1moode1
 
 # Part 3: Kernel package
 # NOTE: Kernel install is skipped if KERNEL_NEW_VER=""
-KERNEL_NEW_VER="6.12.25"
-KERNEL_NEW_PKGVER="1:6.12.25-1+rpt1"
+KERNEL_NEW_VER="6.12.34"
+KERNEL_NEW_PKGVER="1:6.12.34-1+rpt1"
 
 # Initialize step counter
 STEP=0
