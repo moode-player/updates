0884efd7e3adea102ae576283f9372577368e5ff
Tim Curtis <tim@moodeaudio.org>
2026-06-25 11:32:23 -0400

    Bump to WIP r1025

--- moode-player/update10/install.sh
+++ moode-player/update10/install.sh
@@ -9,12 +9,12 @@
 #
 
 # Part 1: In-place update date (same as moOde release date)
-INPLACE_UPDATE_DATE="2026-06-21"
+INPLACE_UPDATE_DATE="2026-MM-DD"
 SQLDB=/var/local/www/db/moode-sqlite3.db
 
 # Part 2: List of package updates (cumulative)
 PKG_UPDATES=(
-moode-player=10.2.4-1moode1
+moode-player=10.2.5-1moode1
 mpd=0.24.12-1moode1
 squeezelite=2.0.0-1541+git20250609.72e1fd8-1moode1
 caps=0.9.26-1moode1
@@ -157,6 +157,9 @@ if [ $KERNEL_NEW_VER != "" ]; then
 fi
 
 # 5 Install package updates
+#
+# TODO: First check if package is already current
+#
 for PACKAGE in "${PKG_UPDATES[@]}"
 do
 	STEP=$((STEP + 1))
